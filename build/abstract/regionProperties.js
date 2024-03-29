f43ccf3550d1427eb55fe2bdfd66c59bbc8964cb
ed3ath <edeath1208@gmail.com>
2024-03-29 08:34:45 +0800

    remove build folder

--- build/abstract/regionProperties.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MapRegionProperties = void 0;
-const rectangle_1 = require("./rectangle");
-class MapRegionProperties extends rectangle_1.Rectangle {
-}
-exports.MapRegionProperties = MapRegionProperties;
--- build/common/configType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ConfigType = void 0;
-var ConfigType;
-(function (ConfigType) {
-    ConfigType[ConfigType["UNKNOWN"] = 0] = "UNKNOWN";
-    ConfigType[ConfigType["JSON"] = 1] = "JSON";
-    ConfigType[ConfigType["YAML"] = 2] = "YAML";
-})(ConfigType || (exports.ConfigType = ConfigType = {}));
